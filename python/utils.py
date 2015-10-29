cdd44e9e918e3014443dc5931214808eb6b14742
aibek76 <aibek.musaev@gmail.com>
2015-10-29 12:49:17 -0400

    update config files

---
+++ python/utils.py
@@ -0,0 +1,14 @@
+def generate_cell(N, E, coef=0.04166666666667):
+    from decimal import Decimal
+
+    if (N==None or N=='None' or E==None or E=='None'):
+        return None
+
+    if coef<0.04166666666667: coef = 0.04166666666667
+    if coef>1: coef = 1
+    row = int(round(Decimal(Decimal(90.0)+Decimal(N))/Decimal(coef)))
+    col = int(round(Decimal(Decimal(180.0)+Decimal(E))/Decimal(coef)))
+    key = str(row)+'_'+str(col)
+
+    return key
+
